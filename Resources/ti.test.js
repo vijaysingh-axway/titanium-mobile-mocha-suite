e1419473471ff7bd8b7b3164321013006244a459
Christopher Williams <chris.a.williams@gmail.com>
2016-06-02 12:43:43 -0400

    Update tests for Titanium module

--- Resources/ti.test.js
+++ Resources/ti.test.js
@@ -6,71 +6,101 @@
  */
 
 var should = require('./should'),
-	utilities = require('./utilities/utilities');
+	utilities = require('./utilities/utilities'),
+	assert = require('./utilities/assertions');
 
 describe('Titanium', function () {
 
-	it('apiName', function (finish) {
+	it('apiName', function () {
 		should(Ti.apiName).be.eql('Ti');
-		finish();
+		should(Ti.apiName).be.a.readOnlyString;
 	});
 
-	it('version', function (finish) {
-		should(Ti.version).be.a.String;
+	it('version', function () {
+		should(Ti.version).not.eql('__TITANIUM_VERSION__');
+		should(Ti.version).be.a.readOnlyString;
+	});
+
+	it('getVersion()', function () {
 		should(Ti.getVersion).be.a.Function;
 		should(Ti.getVersion()).be.a.String;
-		should(Ti.version).not.eql('__TITANIUM_VERSION__');
-		finish();
+		should(Ti.getVersion()).not.eql('__TITANIUM_VERSION__');
 	});
-	it('buildDate', function (finish) {
-		should(Ti.buildDate).be.a.String;
+
+	it('buildDate', function () {
+		should(Ti.buildDate).not.eql('__TITANIUM_BUILD_DATE__');
+		should(Ti.buildDate).be.a.readOnlyString;
+	});
+
+	it('getBuildDate()', function () {
 		should(Ti.getBuildDate).be.a.Function;
 		should(Ti.getBuildDate()).be.a.String;
-		should(Ti.buildDate).not.eql('__TITANIUM_BUILD_DATE__');
-		finish();
+		should(Ti.getBuildDate()).not.eql('__TITANIUM_BUILD_DATE__');
 	});
-	it('buildHash', function (finish) {
-		should(Ti.buildHash).be.a.String;
+
+	it('buildHash', function () {
+		should(Ti.buildHash).not.eql('__TITANIUM_BUILD_HASH__');
+		should(Ti.buildHash).be.a.readOnlyString;
+	});
+
+	it('getBuildHash()', function () {
 		should(Ti.getBuildHash).be.a.Function;
 		should(Ti.getBuildHash()).be.a.String;
-		should(Ti.buildHash).not.eql('__TITANIUM_BUILD_HASH__');
-		finish();
+		should(Ti.getBuildHash()).not.eql('__TITANIUM_BUILD_HASH__');
 	});
-	it('userAgent', function (finish) {
+
+	it('userAgent', function () {
 		should(Ti.userAgent).be.a.String;
+
+		var save = Ti.userAgent;
+		Ti.userAgent = 'Titanium_Mocha_Test';
+		should(Ti.userAgent).be.eql('Titanium_Mocha_Test');
+		Ti.userAgent = save;
+		should(Ti.userAgent).be.eql(save);
+	});
+
+	it('getUserAgent()', function () {
 		should(Ti.getUserAgent).be.a.Function;
 		should(Ti.getUserAgent()).be.a.String;
+	});
+
+	it('setUserAgent()', function () {
 		should(Ti.setUserAgent).be.a.Function;
 		var save = Ti.getUserAgent();
 		Ti.setUserAgent('Titanium_Mocha_Test');
 		should(Ti.getUserAgent()).be.eql('Titanium_Mocha_Test');
+		should(Ti.userAgent).be.eql('Titanium_Mocha_Test');
 		Ti.setUserAgent(save);
 		should(Ti.getUserAgent()).be.eql(save);
-		finish();
+		should(Ti.userAgent).be.eql(save);
 	});
-	it('addEventListener', function (finish) {
+
+	it('addEventListener()', function () {
 		should(Ti.addEventListener).be.a.Function;
-		finish();
 	});
-	it('removeEventListener', function (finish) {
+
+	it('removeEventListener()', function () {
 		should(Ti.removeEventListener).be.a.Function;
-		finish();
 	});
-	it('applyProperties', function (finish) {
+
+	it('applyProperties()', function () {
 		should(Ti.applyProperties).be.a.Function;
 		Ti.mocha_test = undefined;
 		should(Ti.applyProperties({ mocha_test: 'mocha_test_value' }))
 		should(Ti.mocha_test !== undefined);
 		should(Ti.mocha_test).be.eql('mocha_test_value');
 		Ti.mocha_test = undefined;
-		finish();
 	});
-	it('createBuffer', function (finish) {
+
+	it('createBuffer()', function () {
 		should(Ti.createBuffer).be.a.Function;
-		finish();
 	});
-	it('fireEvent', function (finish) {
+
+	it('createProxy()', function () {
+		should(Ti.createProxy).be.a.Function;
+	});
+
+	it('fireEvent()', function () {
 		should(Ti.fireEvent).be.a.Function;
-		finish();
 	});
 });
