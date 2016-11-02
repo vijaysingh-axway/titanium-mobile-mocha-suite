f6e048753b9268e38c70d0358101e6a5912e8216
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2016-11-02 13:15:20 +0100

    [TIMOB-7855] Update unit-tests for new parity

--- Resources/ti.gesture.test.js
+++ Resources/ti.gesture.test.js
@@ -20,8 +20,7 @@ describe('Titanium.Gesture', function () {
 		should(Ti.Gesture.removeEventListener).be.a.Function;
 	});
 
-	// TODO Not defined on iOS, but probably should for parity sake?
-	(utilities.isIOS() ? it.skip : it)('landscape', function () {
+	it('landscape', function () {
 		should(Ti.Gesture).have.a.readOnlyProperty('landscape').which.is.a.Boolean;
 	});
 
@@ -29,37 +28,20 @@ describe('Titanium.Gesture', function () {
 		should(Ti.Gesture).have.a.readOnlyProperty('orientation').which.is.a.Number;
 	});
 
-	// TODO Not defined on iOS, but probably should for parity sake?
-	(utilities.isIOS() ? it.skip : it)('portrait', function () {
+	it('portrait', function () {
 		should(Ti.Gesture).have.a.readOnlyProperty('portrait').which.is.a.Boolean;
 	});
 
-	// TODO Not defined on iOS, but probably should for parity sake?
-	// FIXME Doesn't exist on Android
-	((utilities.isIOS() || utilities.isAndroid()) ? it.skip : it)('getLandscape()', function () {
+	it('getLandscape()', function () {
 		should(Ti.Gesture.getLandscape).not.be.undefined;
 		should(Ti.Gesture.getLandscape).be.a.Function;
 		should(Ti.Gesture.getLandscape()).be.a.Boolean;
 	});
 
-	// TODO Not defined on iOS, but probably should for parity sake?
-	// FIXME Doesn't exist on Android
-	((utilities.isIOS() || utilities.isAndroid()) ? it.skip : it)('getPortrait()', function () {
+	it('getPortrait()', function () {
 		should(Ti.Gesture.getPortrait).not.be.undefined;
 		should(Ti.Gesture.getPortrait).be.a.Function;
 		should(Ti.Gesture.getPortrait()).be.a.Boolean;
-	});
-
-	it('isLandscape()', function () {
-		should(Ti.Gesture.isLandscape).not.be.undefined;
-		should(Ti.Gesture.isLandscape).be.a.Function;
-		should(Ti.Gesture.isLandscape()).be.a.Boolean;
-	});
-
-	it('isPortrait()', function () {
-		should(Ti.Gesture.isPortrait).not.be.undefined;
-		should(Ti.Gesture.isPortrait).be.a.Function;
-		should(Ti.Gesture.isPortrait()).be.a.Boolean;
 	});
 
 	// FIXME Seems like only Windows has this? Was it deprecated/removed?
