2a92283ee5a4c0f46c454a4cae0228d8834b88e7
Christopher Williams <chris.a.williams@gmail.com>
2017-09-25 11:20:03 -0400

    Revert back to correct app.js

--- Resources/app.js
+++ Resources/app.js
@@ -21,84 +21,83 @@ win.open();
 // ============================================================================
 // Add the tests here using "require"
 // ES6 syntax/compatability tests
-require('./fail');
-// require('./es6.arrows.test');
-// require('./es6.default.args.test');
-// require('./es6.rest.args.test');
-// // The JSCore used in Windows doesnt support rest args yet,
-// // avoid requiring it until it dos otherwise it will crash
-// if (!utilities.isWindows()) {
-// 	require('./es6.rest.args.test');
-// }
-// require('./es6.spread.args.test');
-// require('./es6.string.interpolation.test');
-// // Titanium APIs
-// require('./ti.accelerometer.test');
-// require('./ti.analytics.test');
-// require('./ti.api.test');
-// require('./ti.app.test');
-// require('./ti.app.ios.searchquery.test');
-// require('./ti.app.properties.test');
-// require('./ti.app.windows.backgroundservice.test');
-// require('./ti.blob.test');
-// require('./ti.builtin.test');
-// require('./ti.buffer.test');
-// require('./ti.codec.test');
-// require('./ti.contacts.test');
-// require('./ti.contacts.group.test');
-// require('./ti.contacts.person.test');
-// require('./ti.database.test');
-// require('./ti.filesystem.test');
-// require('./ti.filesystem.file.test');
-// require('./ti.filesystem.filestream.test');
-// require('./ti.geolocation.test');
-// require('./ti.gesture.test');
-// require('./ti.internal.test');
-// require('./ti.locale.test');
-// require('./ti.map.test');
-// require('./ti.media.audioplayer.test');
-// require('./ti.media.sound.test');
-// require('./ti.network.test');
-// require('./ti.network.cookie.test');
-// require('./ti.network.httpclient.test');
-// require('./ti.network.socket.tcp.test');
-// require('./ti.network.socket.udp.test');
-// require('./ti.platform.test');
-// require('./ti.platform.displaycaps.test');
-// require('./ti.require.test');
-// require('./ti.stream.test');
-// require('./ti.test');
-// require('./ti.ui.test');
-// require('./ti.ui.2dmatrix.test');
-// require('./ti.ui.activityindicator.test');
-// require('./ti.ui.alertdialog.test');
-// require('./ti.ui.android.drawerlayout.test');
-// require('./ti.ui.button.test');
-// require('./ti.ui.constants.test');
-// require('./ti.ui.emaildialog.test');
-// require('./ti.ui.imageview.test');
-// require('./ti.ui.ios.test');
-// require('./ti.ui.ios.previewcontext.test');
-// require('./ti.ui.label.test');
-// require('./ti.ui.layout.test');
-// require('./ti.ui.listview.test');
-// require('./ti.ui.optiondialog.test');
-// require('./ti.ui.picker.test');
-// require('./ti.ui.progressbar.test');
-// require('./ti.ui.scrollableview.test');
-// require('./ti.ui.scrollview.test');
-// require('./ti.ui.searchbar.test');
-// require('./ti.ui.switch.test');
-// require('./ti.ui.tab.test');
-// require('./ti.ui.tableview.test');
-// require('./ti.ui.textfield.test');
-// require('./ti.ui.toolbar.test');
-// require('./ti.ui.view.test');
-// require('./ti.ui.webview.test');
-// require('./ti.ui.window.test');
-// require('./ti.ui.windows.commandbar.test');
-// require('./ti.utils.test');
-// require('./ti.xml.test');
+require('./es6.arrows.test');
+require('./es6.default.args.test');
+require('./es6.rest.args.test');
+// The JSCore used in Windows doesnt support rest args yet,
+// avoid requiring it until it dos otherwise it will crash
+if (!utilities.isWindows()) {
+	require('./es6.rest.args.test');
+}
+require('./es6.spread.args.test');
+require('./es6.string.interpolation.test');
+// Titanium APIs
+require('./ti.accelerometer.test');
+require('./ti.analytics.test');
+require('./ti.api.test');
+require('./ti.app.test');
+require('./ti.app.ios.searchquery.test');
+require('./ti.app.properties.test');
+require('./ti.app.windows.backgroundservice.test');
+require('./ti.blob.test');
+require('./ti.builtin.test');
+require('./ti.buffer.test');
+require('./ti.codec.test');
+require('./ti.contacts.test');
+require('./ti.contacts.group.test');
+require('./ti.contacts.person.test');
+require('./ti.database.test');
+require('./ti.filesystem.test');
+require('./ti.filesystem.file.test');
+require('./ti.filesystem.filestream.test');
+require('./ti.geolocation.test');
+require('./ti.gesture.test');
+require('./ti.internal.test');
+require('./ti.locale.test');
+require('./ti.map.test');
+require('./ti.media.audioplayer.test');
+require('./ti.media.sound.test');
+require('./ti.network.test');
+require('./ti.network.cookie.test');
+require('./ti.network.httpclient.test');
+require('./ti.network.socket.tcp.test');
+require('./ti.network.socket.udp.test');
+require('./ti.platform.test');
+require('./ti.platform.displaycaps.test');
+require('./ti.require.test');
+require('./ti.stream.test');
+require('./ti.test');
+require('./ti.ui.test');
+require('./ti.ui.2dmatrix.test');
+require('./ti.ui.activityindicator.test');
+require('./ti.ui.alertdialog.test');
+require('./ti.ui.android.drawerlayout.test');
+require('./ti.ui.button.test');
+require('./ti.ui.constants.test');
+require('./ti.ui.emaildialog.test');
+require('./ti.ui.imageview.test');
+require('./ti.ui.ios.test');
+require('./ti.ui.ios.previewcontext.test');
+require('./ti.ui.label.test');
+require('./ti.ui.layout.test');
+require('./ti.ui.listview.test');
+require('./ti.ui.optiondialog.test');
+require('./ti.ui.picker.test');
+require('./ti.ui.progressbar.test');
+require('./ti.ui.scrollableview.test');
+require('./ti.ui.scrollview.test');
+require('./ti.ui.searchbar.test');
+require('./ti.ui.switch.test');
+require('./ti.ui.tab.test');
+require('./ti.ui.tableview.test');
+require('./ti.ui.textfield.test');
+require('./ti.ui.toolbar.test');
+require('./ti.ui.view.test');
+require('./ti.ui.webview.test');
+require('./ti.ui.window.test');
+require('./ti.ui.windows.commandbar.test');
+require('./ti.utils.test');
+require('./ti.xml.test');
 // ============================================================================
 
 // add a special mocha reporter that will time each test run using
