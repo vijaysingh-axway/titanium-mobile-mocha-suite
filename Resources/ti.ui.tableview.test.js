8b0e5aba91cad11ada7efbba889527d3e4e90cfe
Gary Mathews <gmathews@appcelerator.com>
2017-12-20 11:12:52 -0800

    Refactor TIMOB-15765 test for JNI references

--- Resources/ti.ui.tableview.test.js
+++ Resources/ti.ui.tableview.test.js
@@ -1087,47 +1087,32 @@ describe('Titanium.UI.TableView', function () {
 	// Verifies that we don't run into the JNI ref overflow issue on Android
 	// FIXME Eventually crashes on Windows Desktop, crashes right away with no output on Phone
 	it.windowsBroken('TIMOB-15765', function () {
-		var numberOfTableRowsToTest = 400, // 50 is enough to trigger on Android 4.4.2. 400 hits error on Android 6.0/23
-			vAnswerTable,
-			numOfQuestions = numberOfTableRowsToTest / 5,
-			numOfAnswers = numOfQuestions * 4,
-			sections = [],
-			i,
-			questionTableSection,
-			questionRow,
-			j,
-			answerRow,
-			lAnswer,
-			x,
-			y;
-		this.timeout(6e4); // minute
+		var views = [],
+			references = 51200, // JNI max is 51200
+			error;
 
-		vAnswerTable = Ti.UI.createTableView({
-			data: [ Ti.UI.createTableViewRow({ title:'Loading...' }) ],
-		});
+		// create references
+		for (var i = 0; i < references; i++) {
+			views.push(Ti.UI.createView());
 
-		for (i = 0; i < numOfQuestions; i++) {
-			questionTableSection = Ti.UI.createTableViewSection({});
-			questionRow = Ti.UI.createTableViewRow({});
-			questionTableSection.add(questionRow);
-
-			for (j = 0; j < numOfAnswers; j++) {
-				answerRow = Ti.UI.createTableViewRow({});
-				lAnswer = Ti.UI.createLabel({});
-				answerRow.add(lAnswer);
-				questionTableSection.add(answerRow);
-			}
-			sections.push(questionTableSection);
-		}
-
-		// Add the sections created above to the table view
-		vAnswerTable.setData(sections);
-
-		for (x = 0; x < vAnswerTable.data.length; x++) {
-			Ti.API.info('Here after ' + x + ' iterations outer loop. Current section: ' + vAnswerTable.data[x]);
-			for (y = 0; y < vAnswerTable.data[x].rowCount; y++) {
-				Ti.API.info('Here after ' + y + ' iterations inner loop, ' + x + ' iterations outer loop. Current section row: ' +  vAnswerTable.data[x].rows[y]);
+			if (!(i % Math.floor(references / 10))) {
+				Ti.API.info('creating references... ' + i + '/' + references);
 			}
 		}
+
+		// validate references
+		try {
+			should(views.length).be.eql(references);
+
+			for (i = 0; i < references; i++) {
+				should(views[i]).not.be.undefined;
+				should(views[i]).be.an.Object;
+			}
+
+			Ti.API.info('success, created ' + references + ' references!');
+		} catch (e) {
+			error = e;
+		}
+		finish(error);
 	});
 });
