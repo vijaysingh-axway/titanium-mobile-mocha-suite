8969484cd87f6692b26264c4c2bc2a2a545330f7
Ewan Harris <ewanharris93@gmail.com>
2018-02-26 15:49:15 +0000

    Force transpilation for test suite (#47)

--- scripts/test.js
+++ scripts/test.js
@@ -137,6 +137,7 @@ function addTiAppProperties(next) {
 			content.push('\t<property name="presetDouble" type="double">1.23456</property>');
 			content.push('\t<property name="presetInt" type="int">1337</property>');
 			content.push('\t<property name="presetString" type="string">Hello!</property>');
+			content.push('\t<transpile>true</transpile>');
 		} else if (line.indexOf('<android xmlns:android') >= 0) {
 			// Inject the google maps api key
 			content.push('\t\t<manifest>');
