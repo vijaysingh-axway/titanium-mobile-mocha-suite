55c695d4a1db0fed9a72506491d25757a84baebe
Kota Iguchi <developer@infosia.co.jp>
2018-04-18 10:18:33 +0900

    [TIMOB-25096] [TIMOB-25097] [TIMOB-25098] re-enable Ti.Network TLS_VERSION_1_* (#53)

--- Resources/ti.network.test.js
+++ Resources/ti.network.test.js
@@ -32,8 +32,7 @@ describe('Titanium.Network', function () {
 	}
 	for (i = 0; i < TLS_VERSIONS.length; i++) {
 		// FIXME Fails on Android and iOS for some reason! They say they're undefined, not Number
-		// FIXME Windows fails to find the property up the prototype chain in utilities/assertions, line 33
-		it.allBroken(TLS_VERSIONS[i], function () { // eslint-disable-line no-loop-func
+		it.androidAndIosBroken(TLS_VERSIONS[i], function () { // eslint-disable-line no-loop-func
 			should(Ti.Network).have.constant(TLS_VERSIONS[i]).which.is.a.Number;
 		});
 	}
