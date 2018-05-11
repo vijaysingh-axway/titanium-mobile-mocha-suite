3188e20d6686a35ca272e303bf9946baf01d1f63
Hans Knöchel <hansemannn@users.noreply.github.com>
2018-05-11 16:09:37 +0200

    [TIMOB-16878] Android: Refactored ScrollableView

--- Resources/ti.ui.scrollableview.test.js
+++ Resources/ti.ui.scrollableview.test.js
@@ -4,10 +4,25 @@
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
+'use strict';
 var should = require('./utilities/assertions'),
 	utilities = require('./utilities/utilities');
 
 describe('Titanium.UI.ScrollableView', function () {
+	var win;
+	this.timeout(5000);
+
+	afterEach(function (finish) {
+		if (win) {
+			win.addEventListener('close', function () {
+				finish();
+			});
+			win.close();
+			win = null;
+		} else {
+			finish();
+		}
+	});
 
 	it('apiName', function () {
 		var scrollableView = Ti.UI.createScrollableView({});
@@ -21,7 +36,7 @@ describe('Titanium.UI.ScrollableView', function () {
 		should(bar.getViews).be.a.Function;
 		should(bar.views).be.empty;
 		should(bar.getViews()).be.empty;
-		bar.views = [Ti.UI.createView(), Ti.UI.createView()];
+		bar.views = [ Ti.UI.createView(), Ti.UI.createView() ];
 		should(bar.views.length).eql(2);
 		should(bar.getViews().length).eql(2);
 	});
@@ -33,32 +48,64 @@ describe('Titanium.UI.ScrollableView', function () {
 		should(bar.getCurrentPage).be.a.Function;
 		should(bar.currentPage).eql(0);
 		should(bar.getCurrentPage()).eql(0);
-		bar.views = [Ti.UI.createView(), Ti.UI.createView()];
+		bar.views = [ Ti.UI.createView(), Ti.UI.createView() ];
 		bar.currentPage = 1;
 		should(bar.currentPage).eql(1); // Android gives 0
 		should(bar.getCurrentPage()).eql(1);
 	});
 
-	// FIXME calling moveNext() doesn't seem to update currentPage value
-	(utilities.isAndroid() ? it.skip : it)('moveNext', function () {
-		var bar = Ti.UI.createScrollableView({});
-		should(bar.moveNext).be.a.Function;
-		bar.views = [Ti.UI.createView(), Ti.UI.createView()];
-		bar.moveNext();
-		should(bar.currentPage).eql(1);
-		should(bar.getCurrentPage()).eql(1);
-	});
-
-	// FIXME calling moveNext() doesn't seem to update currentPage value
-	(utilities.isAndroid() ? it.skip : it)('movePrevious', function () {
-		var bar = Ti.UI.createScrollableView({});
-		should(bar.movePrevious).be.a.Function;
-		bar.views = [Ti.UI.createView(), Ti.UI.createView()];
-		bar.moveNext();
-		should(bar.currentPage).eql(1); // Android gives 0
-		should(bar.getCurrentPage()).eql(1);
-		bar.movePrevious();
-		should(bar.currentPage).eql(0);
-		should(bar.getCurrentPage()).eql(0);
+	it('moveX-scrollTo', function (finish) {
+		var testName = null,
+			nextPageIndex = 0,
+			bar = null;
+		this.slow(5000);
+		this.timeout(20000);
+		function doNextTest() {
+			try {
+				if (!testName) {
+					testName = 'moveNext';
+					Ti.API.info('Testing ScrollableView.moveNext()');
+					nextPageIndex = bar.currentPage + 1;
+					should(bar.moveNext).be.a.Function;
+					bar.moveNext();
+				} else if (testName === 'moveNext') {
+					testName = 'movePrevious';
+					Ti.API.info('Testing ScrollableView.movePrevious()');
+					nextPageIndex = bar.currentPage - 1;
+					should(bar.movePrevious).be.a.Function;
+					bar.movePrevious();
+				} else if (testName === 'movePrevious') {
+					testName = 'scrollToView';
+					Ti.API.info('Testing ScrollableView.scrollToView()');
+					nextPageIndex = 2;
+					should(bar.scrollToView).be.a.Function;
+					bar.scrollToView(nextPageIndex);
+				} else if (testName === 'scrollToView') {
+					finish();
+				}
+			} catch (err) {
+				finish(err);
+			}
+		}
+		win = Ti.UI.createWindow();
+		bar = Ti.UI.createScrollableView();
+		bar.views = [ Ti.UI.createView(), Ti.UI.createView(), Ti.UI.createView() ];
+		bar.addEventListener('scrollend', function (e) {
+			try {
+				should(e.currentPage).eql(nextPageIndex);
+				should(bar.currentPage).eql(nextPageIndex);
+				should(bar.getCurrentPage()).eql(nextPageIndex);
+				doNextTest();
+			} catch (err) {
+				finish(err);
+			}
+		});
+		win.add(bar);
+		win.addEventListener('postlayout', function () {
+			if (!testName) {
+				doNextTest();
+			}
+		});
+		win.open();
 	});
 });
