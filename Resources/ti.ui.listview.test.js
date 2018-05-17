2229d6ffcc77763e2165a05bcd2c67a404ff9948
Hans Knöchel <hansemannn@users.noreply.github.com>
2018-05-17 13:24:37 +0200

    Update ti.ui.listview.test.js

--- Resources/ti.ui.listview.test.js
+++ Resources/ti.ui.listview.test.js
@@ -868,4 +868,70 @@ describe('Titanium.UI.ListView', function () {
 		win.add(list);
 		win.open();
 	});
+	
+	it.windowsAndAndroidMissing('.refreshControl (in NavigationWindow)', function (finish) {
+		var window, nav, control, listView;
+
+		window = Ti.UI.createWindow({
+			title: 'Hello World',
+			largeTitleEnabled: true,
+			extendEdges: [ Ti.UI.EXTEND_EDGE_ALL ]
+		});
+
+		window.addEventListener('open', function () {
+			control.beginRefreshing();
+		});
+
+		nav = Ti.UI.iOS.createNavigationWindow({
+			window: window
+		});
+
+		control = Ti.UI.createRefreshControl();
+
+		listView = Ti.UI.createListView({
+			refreshControl: control
+		});
+
+		control.addEventListener('refreshstart', function () {
+			setTimeout(function () {
+				control.endRefreshing();
+			}, 2000);
+		});
+
+		control.addEventListener('refreshend', function () {
+			finish();
+		});
+
+		window.add(listView);
+		nav.open();
+	});
+
+	it.windowsMissing('.refreshControl (Basic)', function (finish) {
+		var window, control, listView;
+
+		window = Ti.UI.createWindow();
+
+		window.addEventListener('open', function () {
+			control.beginRefreshing();
+		});
+
+		control = Ti.UI.createRefreshControl();
+
+		listView = Ti.UI.createListView({
+			refreshControl: control
+		});
+
+		control.addEventListener('refreshstart', function () {
+			setTimeout(function () {
+				control.endRefreshing();
+			}, 2000);
+		});
+
+		control.addEventListener('refreshend', function () {
+			finish();
+		});
+
+		window.add(listView);
+		window.open();
+	});
 });
