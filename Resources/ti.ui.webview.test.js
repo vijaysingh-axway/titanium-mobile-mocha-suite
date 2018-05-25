c4cc2855792a0cb2bfc2def9c6decc115332738e
Christopher Williams <chris.a.williams@gmail.com>
2018-05-25 14:58:55 -0400

    Move Ti.UI.WebView.zoomLevel test to suite

--- Resources/ti.ui.webview.test.js
+++ Resources/ti.ui.webview.test.js
@@ -357,4 +357,40 @@ describe('Titanium.UI.WebView', function () {
 		webView.url = url;
 		win.open();
 	});
+
+	it.windowsMissing('.zoomLevel', function (finish) {
+		var webView;
+
+		this.slow(5000);
+		this.timeout(10000);
+
+		win = Ti.UI.createWindow();
+
+		webView = Ti.UI.createWebView({
+			html: '<!DOCTYPE html><html><body><p>HELLO WORLD</p></body></html>'
+		});
+
+		webView.addEventListener('load', function () {
+			should(webView.zoomLevel).be.a.Number;
+			should(webView.getZoomLevel).be.a.Function;
+			should(webView.setZoomLevel).be.a.Function;
+
+			should(webView.zoomLevel).eql(1.0);
+			should(webView.getZoomLevel()).eql(1.0);
+
+			setTimeout(function () {
+				webView.zoomLevel = 3.0;
+				should(webView.zoomLevel).eql(3.0);
+				should(webView.getZoomLevel()).eql(3.0);
+				setTimeout(function () {
+					webView.setZoomLevel(1.0);
+					should(webView.zoomLevel).eql(1.0);
+					finish();
+				}, 500);
+			}, 500);
+		});
+
+		win.add(webView);
+		win.open();
+	});
 });
