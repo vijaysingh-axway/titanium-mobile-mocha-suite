1b716007f4a45ca2308d6d96bacdebd3635267c5
Christopher Williams <chris.a.williams@gmail.com>
2018-10-03 10:20:55 -0400

    Migrate tests from SDK to suite

--- Resources/ti.media.audioplayer.test.js
+++ Resources/ti.media.audioplayer.test.js
@@ -32,11 +32,20 @@ describe('Titanium.Media.AudioPlayer', function () {
 		should(audioPlayer.apiName).be.eql('Ti.Media.AudioPlayer');
 	});
 
-	it('.url', function () {
+	// Updated existing test-case, please replace with old one
+	it('.url', function (finish) {
 		should(audioPlayer.url).be.a.String;
 		should(audioPlayer.getUrl).be.a.Function;
 		should(audioPlayer.setUrl).be.a.Function;
 		should(audioPlayer.url).eql(audioPlayer.getUrl());
+
+		// Re-set URL to test TIMOB-26334, this should not crash
+		try {
+			audioPlayer.url = '/sample.mp3';
+			finish();
+		} catch (e) {
+			finish(e);
+		}
 	});
 
 	it('#start, #stop', function (finish) {
@@ -71,6 +80,22 @@ describe('Titanium.Media.AudioPlayer', function () {
 			audioPlayer.restart();
 			audioPlayer.stop();
 			finish();
+		}, 1000);
+	});
+
+	it('.duration', function (finish) {
+		this.timeout(2000);
+		audioPlayer.start();
+
+		setTimeout(function () {
+			try {
+				should(audioPlayer.duration).be.a.Number;
+				// give a tiny bit of fudge room here. iOS and Android differ by 5ms on this file
+				should(audioPlayer.duration).be.within(45250, 45500); // 45 seconds. iOS gives us 45322, Android gives 45327
+				finish();
+			} catch (e) {
+				finish(e);
+			}
 		}, 1000);
 	});
 });
