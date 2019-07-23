427b8396771da8e2aa5ec702c065a79fb2e99fe2
Kota Iguchi <developer@infosia.co.jp>
2019-07-23 15:55:04 +0900

    test: skip test that crashes Windows (#177)

--- Resources/ti.map.test.js
+++ Resources/ti.map.test.js
@@ -161,7 +161,7 @@ describe('Titanium.Map', function () {
 		should(Map).have.constant('TERRAIN_TYPE').which.is.a.Number;
 	});
 
-	it('#createAnnotation()', function () {
+	it.windowsBroken('#createAnnotation()', function () {
 		should(Map.createAnnotation).be.a.Function;
 
 		win = Ti.UI.createWindow();
