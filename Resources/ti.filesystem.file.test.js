952f7651d8d2c502430ff5109bcfa38c0b037ef6
Chris Williams <chris.a.williams@gmail.com>
2019-10-03 10:09:20 -0400

    test(file): add test for TIMOB-27193

--- Resources/ti.filesystem.file.test.js
+++ Resources/ti.filesystem.file.test.js
@@ -796,4 +796,14 @@ describe('Titanium.Filesystem.File', function () {
 			Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory).setRemoteBackup(false);
 		}).not.throw();
 	});
+
+	it.android('TIMOB-27193', () => {
+		const filename = `TIMOB-27193_${Date.now()}.txt`;
+		const file = Ti.Filesystem.getFile(Ti.Filesystem.tempDirectory, filename);
+		const originalPath = file.nativePath;
+		file.createFile();
+		should(file.exists()).eql(true);
+		// make sure we're not getting swindled by having the underlying file inside the proxy get changed on us!
+		should(file.nativePath).eql(originalPath);
+	});
 });
