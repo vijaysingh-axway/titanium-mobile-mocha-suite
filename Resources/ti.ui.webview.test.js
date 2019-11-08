6e22b38ab425b76473f56077a7b631f064bf5964
Chris Williams <chris.a.williams@gmail.com>
2019-11-08 12:17:42 -0500

    test(ui): migrate ti.ui.webview addon tests

--- Resources/ti.ui.webview.test.js
+++ Resources/ti.ui.webview.test.js
@@ -612,4 +612,44 @@ describe('Titanium.UI.WebView', function () {
 		win.add(webView);
 		win.open();
 	});
+
+	it('baseURL should be accessible via window.location', (done) => {
+		const win = Ti.UI.createWindow();
+		const baseURL = 'https://www.google.com/';
+		const webView = Ti.UI.createWebView();
+		webView.setHtml(
+			'<html><body></body></html>',
+			{
+				baseURL
+			}
+		);
+		webView.addEventListener('load', () => {
+			webView.evalJS('window.location.href', result => {
+				try {
+					should(result.replace(/"/g, '')).be.eql(baseURL); // Android encloses the URL in quotes, not sure why.
+				} catch (err) {
+					return done(err);
+				} finally {
+					win.close();
+				}
+				done();
+			});
+		});
+		win.add(webView);
+		win.open();
+	});
+
+	it('basic authentication should work properly', (done) => {
+		const win = Ti.UI.createWindow();
+		const webView = Ti.UI.createWebView({
+			url: 'https://jigsaw.w3.org/HTTP/Basic/'
+		});
+		webView.setBasicAuthentication('guest', 'guest');
+		webView.addEventListener('load', () => {
+			win.close();
+			done();
+		});
+		win.add(webView);
+		win.open();
+	});
 });
