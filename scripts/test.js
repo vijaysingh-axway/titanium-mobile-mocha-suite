548ed965198b83131fc56f653b1eaf26d71b50b0
Gary Mathews <gmathews@appcelerator.com>
2019-12-17 14:58:26 -0800

    fix: device name regex

--- scripts/test.js
+++ scripts/test.js
@@ -360,7 +360,7 @@ function handleBuild(prc, next) {
 	}
 
 	function getDeviceName(token) {
-		const matches = /^[\s\b]+\[(.+)\]\s/g.exec(token.substring(token.indexOf(':') + 1));
+		const matches = /^[\s\b]+\[([^\]]+)\]\s/g.exec(token.substring(token.indexOf(':') + 1));
 		if (matches && matches.length === 2) {
 			return matches[1];
 		}
