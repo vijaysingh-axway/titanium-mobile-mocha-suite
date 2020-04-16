48692caa3807e705538aa02cba750d9ce34702f5
Chris Williams <chris.a.williams@gmail.com>
2020-04-16 11:54:18 -0400

    test: migrate updated test from sdk

--- Resources/ti.test.js
+++ Resources/ti.test.js
@@ -7,68 +7,61 @@
 /* eslint-env mocha */
 /* eslint no-unused-expressions: "off" */
 'use strict';
-var should = require('./utilities/assertions');
+const should = require('./utilities/assertions');
 
-describe('Titanium', function () {
-
-	it('apiName', function () {
+describe('Titanium', () => {
+	it('apiName', () => {
 		should(Ti).have.readOnlyProperty('apiName').which.is.a.String;
 		should(Ti.apiName).be.eql('Ti');
 	});
 
-	// FIXME Get working on IOS!
-	it.iosBroken('version', function () {
-		should(Ti.version).not.eql('__TITANIUM_VERSION__');
+	it('version', () => {
 		should(Ti).have.readOnlyProperty('version').which.is.a.String;
+		should(Ti.version).not.eql('__VERSION__'); // This is the placeholder value used in iOS, let's ensure we replaced it!
+		should(Ti.version).match(/\d+\.\d+\.\d+/); // i.e. '9.0.0' (the short version string, no timestamp qualifier)
 	});
 
-	it('#getVersion()', function () {
+	it('#getVersion()', () => {
 		should(Ti.getVersion).be.a.Function;
-		should(Ti.getVersion()).be.a.String;
-		should(Ti.getVersion()).not.eql('__TITANIUM_VERSION__');
-		// TODO Test format of the version string. what should we expect? Something like: /\d\.\d\.\d(\.(v\d+|GA))/
+		should(Ti.getVersion()).eql(Ti.version);
 	});
 
-	// FIXME Get working on IOS!
-	it.iosBroken('buildDate', function () {
-		should(Ti.buildDate).not.eql('__TITANIUM_BUILD_DATE__');
+	it('buildDate', () => {
 		should(Ti).have.readOnlyProperty('buildDate').which.is.a.String;
-		// TODO Test format of the date string. what should we expect? Android gives us: '2016/06/02 08:45'
+		should(Ti.buildDate).not.eql('__TIMESTAMP__'); // This is the placeholder value used in iOS, let's ensure we replaced it!
+		should(Ti.buildDate).match(/[01]?\d\/[0123]\d\/20\d{2} \d{2}:\d{2}/); // i.e. '4/14/2020 18:48'
 	});
 
-	it('#getBuildDate()', function () {
+	it('#getBuildDate()', () => {
 		should(Ti.getBuildDate).be.a.Function;
-		should(Ti.getBuildDate()).be.a.String;
-		should(Ti.getBuildDate()).not.eql('__TITANIUM_BUILD_DATE__');
+		should(Ti.getBuildDate()).eql(Ti.buildDate);
 	});
 
-	// FIXME Get working on IOS!
-	it.iosBroken('buildHash', function () {
-		should(Ti.buildHash).not.eql('__TITANIUM_BUILD_HASH__');
+	it('buildHash', () => {
 		should(Ti).have.readOnlyProperty('buildHash').which.is.a.String;
-		// TODO Test format of the buildHash string. what should we expect? Android gives us: 'c012548'
+		should(Ti.buildHash).not.eql('__GITHASH__'); // This is the placeholder value used in iOS, let's ensure we replaced it!
+		should(Ti.buildHash).match(/[a-f0-9]{10}/); // 10-character git sha
 	});
 
-	it('#getBuildHash()', function () {
+	it('#getBuildHash()', () => {
 		should(Ti.getBuildHash).be.a.Function;
-		should(Ti.getBuildHash()).be.a.String;
-		should(Ti.getBuildHash()).not.eql('__TITANIUM_BUILD_HASH__');
+		should(Ti.getBuildHash()).eql(Ti.buildHash);
 	});
 
 	// FIXME File a ticket in JIRA. Updating V8 fixes the property read/write issues, but exposes bug in that we set userAgent as read-only on Android and it shouldn't be
 	it.androidBroken('userAgent', function () {
 		should(Ti.userAgent).be.a.String;
 
-		var save = Ti.userAgent;
+		const save = Ti.userAgent;
 		Ti.userAgent = 'Titanium_Mocha_Test';
 		should(Ti.userAgent).be.eql('Titanium_Mocha_Test');
 		Ti.userAgent = save;
 		should(Ti.userAgent).be.eql(save);
 	});
 
-	it('#getUserAgent()', function () {
+	it('#getUserAgent()', () => {
 		should(Ti.getUserAgent).be.a.Function;
-		should(Ti.getUserAgent()).be.a.String;
+		should(Ti.getUserAgent()).eql(Ti.userAgent);
 	});
 
 	// FIXME Get working on IOS/Android!
@@ -83,13 +76,9 @@ describe('Titanium', function () {
 		should(Ti.userAgent).be.eql(save);
 	});
 
-	it('#addEventListener()', function () {
-		should(Ti.addEventListener).be.a.Function;
-	});
+	it('#addEventListener()', () => should(Ti.addEventListener).be.a.Function);
 
-	it('#removeEventListener()', function () {
-		should(Ti.removeEventListener).be.a.Function;
-	});
+	it('#removeEventListener()', () => should(Ti.removeEventListener).be.a.Function);
 
 	// FIXME Get working on IOS/Android!
 	it.androidAndIosBroken('#applyProperties()', function () {
@@ -101,17 +90,10 @@ describe('Titanium', function () {
 		Ti.mocha_test = undefined;
 	});
 
-	it('#createBuffer()', function () {
-		should(Ti.createBuffer).be.a.Function;
-	});
+	it('#createBuffer()', () => should(Ti.createBuffer).be.a.Function);
 
-	// FIXME Is this really a method we want to expose on our API? Seems like it shouldn't be
-	// Undefined on Android, but there on iOS!
-	it.skip('#createProxy()', function () { // eslint-disable-line mocha/no-skipped-tests
-		should(Ti.createProxy).be.a.Function;
-	});
+	// FIXME Is this really a method we want to expose on our API?
+	it('#createProxy()', () => should(Ti.createProxy).be.a.Function);
 
-	it('#fireEvent()', function () {
-		should(Ti.fireEvent).be.a.Function;
-	});
+	it('#fireEvent()', () => should(Ti.fireEvent).be.a.Function);
 });
