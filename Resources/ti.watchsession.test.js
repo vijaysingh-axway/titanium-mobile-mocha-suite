8c168805da58f119a053c3ddc72c6b12a43f92c3
Chris Williams <chris.a.williams@gmail.com>
2020-04-17 10:12:51 -0400

    test: fix watchsession property type assertion

--- Resources/ti.watchsession.test.js
+++ Resources/ti.watchsession.test.js
@@ -7,58 +7,45 @@
 /* eslint-env mocha */
 /* eslint no-unused-expressions: "off" */
 'use strict';
-var should = require('./utilities/assertions');
+const should = require('./utilities/assertions');
 
 describe.ios('Titanium.WatchSession', function () {
 
-	it('apiName', function () {
-		var watchSession = Ti.WatchSession;
+	it('.apiName', () => {
+		const watchSession = Ti.WatchSession;
 		should(watchSession).have.readOnlyProperty('apiName').which.is.a.String();
 		should(watchSession.apiName).be.eql('Ti.WatchSession');
 	});
 
 	// constants
-	it('ACTIVATION_STATE_NOT_ACTIVATED', function () {
+	it('.ACTIVATION_STATE_NOT_ACTIVATED', () => {
 		should(Ti.WatchSession).have.constant('ACTIVATION_STATE_NOT_ACTIVATED').which.is.a.Number();
 	});
 
-	it('ACTIVATION_STATE_INACTIVE', function () {
+	it('.ACTIVATION_STATE_INACTIVE', () => {
 		should(Ti.WatchSession).have.constant('ACTIVATION_STATE_INACTIVE').which.is.a.Number();
 	});
 
-	it('ACTIVATION_STATE_ACTIVATED', function () {
+	it('.ACTIVATION_STATE_ACTIVATED', () => {
 		should(Ti.WatchSession).have.constant('ACTIVATION_STATE_ACTIVATED').which.is.a.Number();
 	});
 
-	it('#isSupported', function () {
-		should(Ti.WatchSession.isSupported).be.a.Boolean();
-	});
+	// properties
+	it('.isSupported', () => should(Ti.WatchSession.isSupported).be.a.Boolean());
 
-	it('#isPaired', function () {
-		should(Ti.WatchSession.isPaired).be.a.Boolean();
-	});
+	it('.isPaired', () => should(Ti.WatchSession.isPaired).be.a.Boolean());
 
-	it('#isWatchAppInstalled', function () {
-		should(Ti.WatchSession.isWatchAppInstalled).be.a.Boolean();
-	});
+	it('.isWatchAppInstalled', () => should(Ti.WatchSession.isWatchAppInstalled).be.a.Boolean());
 
-	it('#isComplicationEnabled', function () {
-		should(Ti.WatchSession.isComplicationEnabled).be.a.Boolean();
-	});
+	it('.isComplicationEnabled', () => should(Ti.WatchSession.isComplicationEnabled).be.a.Boolean());
 
-	it('#isReachable', function () {
-		should(Ti.WatchSession.isReachable).be.a.Boolean();
-	});
+	it('.isReachable', () => should(Ti.WatchSession.isReachable).be.a.Boolean());
 
-	it('#isActivated', function () {
-		should(Ti.WatchSession.isActivated).be.a.Boolean();
-	});
+	it('.isActivated', () => should(Ti.WatchSession.isActivated).be.a.Boolean());
 
-	it('#hasContentPending', function () {
-		should(Ti.WatchSession.hasContentPending).be.a.Boolean();
-	});
+	it('.hasContentPending', () => should(Ti.WatchSession.hasContentPending).be.a.Boolean());
 
-	it('#remainingComplicationUserInfoTransfers', function () {
-		should(Ti.WatchSession.remainingComplicationUserInfoTransfers).be.a.Number();
+	it('.remainingComplicationUserInfoTransfers', () => {
+		should(Ti.WatchSession.remainingComplicationUserInfoTransfers).be.a.Boolean();
 	});
 });
